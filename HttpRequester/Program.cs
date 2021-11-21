c0fb629d71560b44dff72378b48c3ca1b90e9d8b
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-11-21 17:31:50 +0200

    Add files via upload

---
+++ HttpRequester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpRequester
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage message = await client.GetAsync("http://www.swu.bg/university-profile/structure/faculties.aspx");
+
+            string result = await message.Content.ReadAsStringAsync();
+
+            Console.WriteLine(result);
+        }
+    }
+}
