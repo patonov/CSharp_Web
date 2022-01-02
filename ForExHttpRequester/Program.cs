e4d65374c96454cf8881ae5d14a670d2d8ae05e7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-01-02 22:31:52 +0200

    Add files via upload

---
+++ ForExHttpRequester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ForExHttpRequester
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage message = 
+                await client.GetAsync("https://fcsapi.com/api-v3/forex/latest?symbol=EUR/USD&access_key=R0zdzWGUdSz0xu5iIzhZl");
+
+            string result = await message.Content.ReadAsStringAsync();
+
+            Console.WriteLine(result);
+        }
+    }
+}
