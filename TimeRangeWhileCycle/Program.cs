ab12a9df430104d50c75f9279248ef9e04365ffa
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-01-03 19:21:31 +0200

    Add files via upload

---
+++ TimeRangeWhileCycle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimeRangeWhileCycle
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var startTime = DateTime.Now;
+            var endTime = startTime.AddHours(3);
+            var currentTime = startTime;
+
+            HttpClient client = new HttpClient();
+            var future = startTime.AddSeconds(30);
+
+            while (currentTime <= endTime)
+            {
+                if (DateTime.Now.ToString() == future.ToString())
+                {
+                    HttpResponseMessage message =
+                     await client.GetAsync("https://fcsapi.com/api-v3/forex/latest?symbol=EUR/USD&access_key=R0zdzWGUdSz0xu5iIzhZl");
+
+                    string result = await message.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(result);
+                    Console.WriteLine("==============================================");
+
+                    future = currentTime.AddSeconds(30);
+
+                }
+                currentTime = DateTime.Now;
+
+            }
+
+
+        }
+    }
+}
