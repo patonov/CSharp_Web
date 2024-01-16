f42b131ddf5df4c905574f69f729b4f538bd1f6e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-01-16 19:53:54 +0200

    Add files via upload

---
+++ WebAppOne/WebAppOne/Pages/MainTextContents.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppOne.Pages
+{
+    public class MainTextContentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
