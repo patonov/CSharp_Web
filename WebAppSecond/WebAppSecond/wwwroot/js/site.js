0220d13a524acad22e9bcc43761c44382227afc2
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-02-01 17:42:19 +0200

    Add files via upload

---
+++ WebAppSecond/WebAppSecond/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿
+function attachEvents() {
+
+    document.querySelector("#btnLoadPosts").addEventListener("click", loadSinglePost);
+
+}
+
+async function loadSinglePost() {
+        let comment = document.querySelector("#post");
+        comment.innerHTML = "";
+
+        const item = document.createElement("li");
+        item.textContent = "Tralalaaa";
+
+        comment.appendChild(item);
+}
+
+attachEvents();
+
