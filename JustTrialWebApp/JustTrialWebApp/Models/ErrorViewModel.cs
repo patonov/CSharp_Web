076c3349058bf4b3736bf2ef7473a4be4324fd3a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-02-08 19:49:53 +0200

    Add files via upload

---
+++ JustTrialWebApp/JustTrialWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JustTrialWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
