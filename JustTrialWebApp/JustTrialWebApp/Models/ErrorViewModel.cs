e0ec30497fb475142215c7d76659f1a3345e9577
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-02-09 21:13:39 +0200

    Delete JustTrialWebApp directory

--- JustTrialWebApp/JustTrialWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JustTrialWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
