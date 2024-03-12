f650fd1f1f2c812a167a17b23c04a9f73bef0057
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-03-12 20:36:54 +0200

    Add files via upload

---
+++ Eventures/Eventures.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventures.App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
