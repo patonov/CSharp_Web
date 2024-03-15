2781d90671439ec0e1d7e0b99314578496225389
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-03-15 21:05:14 +0200

    Add files via upload

---
+++ Eventures/Eventures.App/Domain/EventuresUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Eventures.App.Domain
+{
+    public class EventuresUser : IdentityUser
+    {
+
+    }
+}
--- Eventures/Eventures.App/Program.cs
+++ Eventures/Eventures.App/Program.cs
@@ -1,4 +1,5 @@
 using Eventures.App.Data;
+using FluentAssertions.Common;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +20,17 @@ namespace Eventures.App
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 7;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredUniqueChars = 0;
+            });
 
             var app = builder.Build();
 
