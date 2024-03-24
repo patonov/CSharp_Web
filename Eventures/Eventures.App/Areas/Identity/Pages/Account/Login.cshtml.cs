db321cfb6693e38fa985b33d5d8cd38fbcf1debd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-03-24 18:26:32 +0200

    Add files via upload

--- Eventures/Eventures.App/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Eventures/Eventures.App/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -64,26 +64,14 @@ namespace Eventures.App.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            public string Username { get; set; }
+                        
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+                        
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
@@ -115,7 +103,7 @@ namespace Eventures.App.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
