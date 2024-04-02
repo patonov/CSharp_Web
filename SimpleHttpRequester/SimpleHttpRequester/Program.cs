357f76d7c0d2f3004ed4fd685cc16146e8c43df2
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-04-02 23:39:20 +0300

    Add files via upload

---
+++ SimpleHttpRequester/SimpleHttpRequester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace SimpleHttpRequester
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "MyConsoleBrowser/1.0");
+
+            HttpResponseMessage message = await client.GetAsync("https://www.swu.bg/bg/");
+
+            string content = await message.Content.ReadAsStringAsync();
+
+            //Console.WriteLine(content);
+
+            File.WriteAllText("../../../index.html", content);
+
+        }
+    }
+}
