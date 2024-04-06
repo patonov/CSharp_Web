b681c2d3578a51a1c0f486c40658f1d075dcedf5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-04-06 17:29:40 +0300

    Add files via upload

---
+++ BookLibraryApp/BookLibraryApp/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLibraryApp.Models;
+
+public partial class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
---
+++ BookLibraryApp/BookLibraryApp/Models/BookLibraryContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibraryApp.Models;
+
+public partial class BookLibraryContext : DbContext
+{
+    public BookLibraryContext()
+    {
+    }
+
+    public BookLibraryContext(DbContextOptions<BookLibraryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=.;Database=BookLibrary;Integrated Security=True;Encrypt=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Book__3214EC0745D79141");
+
+            entity.ToTable("Book");
+
+            entity.Property(e => e.Author).HasMaxLength(50);
+            entity.Property(e => e.Description).HasMaxLength(250);
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
