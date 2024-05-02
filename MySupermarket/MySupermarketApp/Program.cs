fe098386d3ef96263d8cc35b7e4b7b64d6123e06
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-05-02 16:11:46 +0300

    Add files via upload

--- MySupermarket/MySupermarketApp/Program.cs
+++ MySupermarket/MySupermarketApp/Program.cs
@@ -5,9 +5,16 @@ namespace MySupermarketApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddControllersWithViews();
+            
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
