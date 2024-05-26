7ef91df1b20825ebcdc2ba415441a740c71e17e4
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-05-26 20:45:37 +0300

    Add files via upload

---
+++ GameStore/GameStoreFrontEnd/Models/GameSummary.cs
@@ -0,0 +1,15 @@
+﻿namespace GameStoreFrontEnd.Models
+{
+    public class GameSummary
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
