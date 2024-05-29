bc7edd093ff9393a8e3c2da706f69211fae0eb3a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-05-29 20:37:58 +0300

    Add files via upload

---
+++ GameStore/GameStoreFrontEnd/Clients/GameClient.cs
@@ -0,0 +1,21 @@
+﻿using GameStoreFrontEnd.Models;
+
+namespace GameStoreFrontEnd.Clients
+{
+    public class GameClient
+    {
+        private readonly List<GameSummary> games =
+        [
+            new ()
+            {
+                Id = 1, Name = "Street Fighter", Genre = "Fighting", Price = 19.99M, ReleaseDate = new DateOnly(1992, 7, 15)
+            },
+            new ()
+            {
+             Id = 2, Name = "Counter Strike", Genre = "Roleplaying", Price = 29.99M, ReleaseDate = new DateOnly(1999, 9, 19)
+            }
+        ];
+
+        public GameSummary[] GetGames() => [.. games];
+    }
+}
