b446f7237fdfbf65e7adc307853d5d3903845d49
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-06-02 14:53:02 +0300

    Add files via upload

---
+++ PersonMVC/PersonMVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using PersonMVC.Models;
+using System.Diagnostics;
+
+namespace PersonMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Person person)
+        { 
+            return RedirectToAction("Index", person);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PersonMVC/PersonMVC/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonMVC.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        
+        public string Gender { get; set; } = null!;
+
+        public string Address { get; set; } = null!; 
+    }
+}
