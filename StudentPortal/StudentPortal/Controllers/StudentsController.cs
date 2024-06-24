defd8a19a88d791d0e70557aa4ef70fe38d52bd5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-06-24 22:37:57 +0300

    Add files via upload

---
+++ StudentPortal/StudentPortal/Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Data;
+using StudentPortal.Models;
+
+namespace StudentPortal.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly StdentPortalDbContext dbContext;
+
+        public StudentsController(StdentPortalDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel addStudent)
+        {
+            var student = new Student
+            {
+                Name = addStudent.Name,
+                Email = addStudent.Email,
+                Phone = addStudent.Phone,
+                Subscribed = addStudent.Subscribed,
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+    }
+}
---
+++ StudentPortal/StudentPortal/Data/StdentPortalDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.Models;
+
+namespace StudentPortal.Data
+{
+    public class StdentPortalDbContext : DbContext
+    {
+        public StdentPortalDbContext(DbContextOptions<StdentPortalDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
