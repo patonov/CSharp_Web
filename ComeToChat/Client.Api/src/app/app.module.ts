41d44b762e32045b055d6263bffe577612fa1f0f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-07-10 21:28:41 +0300

    Add files via upload

--- ComeToChat/Client.Api/src/app/app.module.ts
+++ ComeToChat/Client.Api/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
--- ComeToChat/Client.Api/src/app/home/home.component.ts
+++ ComeToChat/Client.Api/src/app/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  userform: FormGroup = new FormGroup({});
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {  
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.userform = this.formBuilder.group({
+      name: ['', Validators.required, Validators.minLength(3), Validators.maxLength(15)]
+    })
+  }
+
+  submitForm(){
+    this.submitted = true;
+
+    if(this.userform.valid){
+      console.log(this.userform.value);
+    }
+  }
 
 }
