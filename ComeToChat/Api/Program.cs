9877700138f9abe47d8c8c0c6232682754f1a843
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-07-13 19:17:27 +0300

    Add files via upload

--- ComeToChat/Api/Program.cs
+++ ComeToChat/Api/Program.cs
@@ -17,9 +17,12 @@ namespace Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSingleton<ChatService>();
             builder.Services.AddSignalR();
+            builder.Services.AddCors();
             
             var app = builder.Build();
-                       
+            
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200"));
+            
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
